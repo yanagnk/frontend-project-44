4e33c90c948d89972a19f589eb90cc7e308b3d11
Yana Gunko <yana.gunko@gmail.com>
2023-06-14 23:23:20 +0200

    Step 3

---
+++ src/cli.js
@@ -0,0 +1,8 @@
+import readlineSync from 'readline-sync';
+
+const salutation = () => {
+const userName = readlineSync.question('May I have your name? ');
+console.log(`Hello, ${userName}!`);
+};
+
+export { salutation };
