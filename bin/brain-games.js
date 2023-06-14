af1ce44d888444199dc6b250436d5ad2251d9ee1
Yana Gunko <yana.gunko@gmail.com>
2023-06-15 01:22:30 +0200

    Step 4

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
-import { salutation } from '../src/cli.js';
+import { salutation } from '../src/cli';
 
+// eslint-disable-next-line no-console
 console.log('Welcome to the Brain Games!');
 
 salutation();
--- src/cli.js
+++ src/cli.js
@@ -1,8 +1,10 @@
 import readlineSync from 'readline-sync';
 
 const salutation = () => {
-const userName = readlineSync.question('May I have your name? ');
-console.log(`Hello, ${userName}!`);
+  const userName = readlineSync.question('May I have your name? ');
+  // eslint-disable-next-line no-console
+  console.log(`Hello, ${userName}!`);
 };
 
+// eslint-disable-next-line import/prefer-default-export
 export { salutation };
