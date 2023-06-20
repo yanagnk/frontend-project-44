3ba957cf713c2d5d43039ab964649641e658d6ba
Yana Gunko <yana.gunko@gmail.com>
2023-06-20 15:47:11 +0200

    loop added, step 5

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,32 +1,30 @@
 import readlineSync from 'readline-sync';
+import { salutation } from '../src/cli.js';
+
+const startGame = () => {
+
+console.log('Answer "yes" if the number is even, otherwise answer "no".');
+
+for (let i = 1; i <= 3; i += 1) {
 
 const number = Math.floor(Math.random() * 101);
 const isCorrect = (num) => (num % 2 === 0) ? 'yes' : 'no';
 const correctAnswer = isCorrect(number);
 
-
-const startGame = () => {
-
-    console.log('Answer "yes" if the number is even, otherwise answer "no".');
-
     const userAnswer = readlineSync.question(`Question: ${number}
 Your answer: `);
 
-    const startRound = () => {
-
-    
-        
-        if (userAnswer === correctAnswer) {
+    if (userAnswer === correctAnswer) {
               console.log ('Correct!');
-            } 
-            return false;
-        };
-
-        if (!startRound()) {
-        return (`${userAnswer} is wrong answer ;(. Correct answer was ${correctAnswer}. Let's try again, USERNAME!`);
-    }
-return 'Congratulations, USERNAME!';
+            } else {
+            console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.
+Let's try again, ${userName}!`);
+       return;
+            }
+        }
+    console.log(`Congratulations,${userName}!`);
+    return;
 }
 
-startGame();
+export { startGame };
 
--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,7 +1,10 @@
 #!/usr/bin/env node
 import { salutation } from '../src/cli.js';
+import { startGame } from './brain-even.js';
 
 // eslint-disable-next-line no-console
 console.log ('Welcome to the Brain Games!');
 
 salutation();
+
+startGame();
