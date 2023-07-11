f0ca20d874307b01861bd0b3901f274e987c68f2
Yana Gunko <yana.gunko@gmail.com>
2023-07-11 13:46:24 +0200

    fixed gcd

--- src/games/brain-gcd.js
+++ src/games/brain-gcd.js
@@ -3,20 +3,21 @@ import randomNumber from '../utils.js';
 
 const taskGcd = 'Find the greatest common divisor of given numbers.';
 
+const getGcd = () => {
+  let x;
+  let y;
+
+  while (y !== 0) {
+    const remainder = x % y;
+    x = y;
+    y = remainder;
+  }
+  return x;
+};
+
 const getGcdRoundData = () => {
   const number1 = randomNumber(1, 100);
   const number2 = randomNumber(1, 100);
-
-  const getGcd = (num1, num2) => {
-    let x = num1;
-    let y = num2;
-    while (y !== 0) {
-      const remainder = x % y;
-      x = y;
-      y = remainder;
-    }
-    return x;
-  };
 
   const correctAnswerGcd = String(getGcd(number1, number2));
 
